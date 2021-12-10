372b28908022eb0adb65fecd6db76a61ef4dac33
Carlos Valiente <carlos.valiente@ecmwf.int>
2021-12-10 21:16:28 +0100

    Nix-based direnv support.

---
+++ shell.nix
@@ -0,0 +1,30 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    # Needed to install uWSGI.
+    libxml2
+    ncurses
+    pcre
+
+    # Install Python 2.7 runtime for tests.
+    python27
+
+    # Install Python 3.8 runtime for tests and build tools.
+    (python38.withPackages
+      (ps: with ps ; [
+        tox
+      ]))
+
+
+    # Niv (https://github.com/nmattia/niv), to keep dependencies up-to-date.
+    #
+    # (`niv` on macOS M1 cannot be installed from 21.05)
+    # niv
+
+    # Keep this line if you use bash.
+    bashInteractive
+  ];
+}
